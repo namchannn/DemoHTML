48ae7f0b5f4a58d0381e7990b145c6ad579b5ad3
Nam Chan <namanhseoul@gmail.com>
2023-07-27 22:30:39 +0700

    Update login.js

--- Practice 2/js/login.js
+++ Practice 2/js/login.js
@@ -1,9 +1,10 @@
+// login js
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-
+    // Declare
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
+    // Check Account
     if (username === 'admin' && password === 'admin') {
         window.location.href = 'nextpage.html';
     } else {
